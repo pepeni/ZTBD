3b453790562dc17730c2805487026e7c04c43c6b
Konrad Ziaja <pepeni1415@gmail.com>
2024-03-29 10:21:28 +0100

    Configured cassandra and mongodb on docker and added test scripts

--- Cassandra.py
+++ Cassandra.py
@@ -1,10 +1,43 @@
 from cassandra.cluster import Cluster
-from options import HOST, PORT
+from options import CASSANDRA_HOST, CASSANDRA_PORT
 
-cluster = Cluster([HOST], port=PORT)
-session = cluster.connect("store")
+cluster = Cluster([CASSANDRA_HOST], port=CASSANDRA_PORT)
+session = cluster.connect()
 
-rows = session.execute('SELECT * FROM shopping_cart;')
+keyspace = 'ztbd'
+
+keyspaces = session.execute("SELECT * FROM system_schema.keyspaces WHERE keyspace_name = %s", (keyspace,))
+if not keyspaces:
+    # Tworzenie nowej przestrzeni kluczy, jeśli nie istnieje
+    session.execute("CREATE KEYSPACE IF NOT EXISTS %s WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}" % keyspace)
+session.set_keyspace(keyspace)
+
+create_table_query = """
+    CREATE TABLE IF NOT EXISTS users (
+        user_id TEXT PRIMARY KEY,
+        username TEXT,
+        email TEXT,
+        age INT
+    );
+"""
+session.execute(create_table_query)
+
+insert_user_query = """
+    INSERT INTO users (user_id, username, email, age)
+    VALUES (%s, %s, %s, %s)
+"""
+
+user_id = 'jan_kowalski2'  # Ciąg znaków jako klucz główny
+username = 'Jan Kowalski2'
+email = 'jan.kowalski@examplee.com'
+age = 31
+
+# Wykonanie zapytania wstawiania
+session.execute(insert_user_query, (user_id, username, email, age))
+
+rows = session.execute('SELECT * FROM users;')
 
 for row in rows:
     print(row)
+
+cluster.shutdown()
