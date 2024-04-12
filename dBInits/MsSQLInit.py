a2078e3d3372507a63017afd858a655c9a6ad874
Konrad Ziaja <pepeni1415@gmail.com>
2024-04-12 11:23:27 +0200

    Added all tables to MSSQLServer db

--- dBInits/MsSQLInit.py
+++ dBInits/MsSQLInit.py
@@ -1,6 +1,6 @@
 import pyodbc as odbc
 from options import DRIVER_NAME, SERVER_NAME, DATABASE_NAME
-from TableScripts.msSqlTables import area_table, crime_table
+from TableScripts.msSqlTables import area_table, crime_table, victim_table, permis_table, weapon_table, status_table, crime_register_table
 
 
 def msSqlInit():
@@ -20,7 +20,7 @@ def msSqlInit():
 
         # Sprawdzam, czy istnieją tabele
 
-        list_of_table_name = {'Area': area_table, 'Crime': crime_table}
+        list_of_table_name = {'Area': area_table, 'Crime': crime_table, 'Victim': victim_table, 'Permis': permis_table, 'Weapon': weapon_table, 'Status': status_table, 'CrimeRegister': crime_register_table}
         for i in list_of_table_name.keys():
             cursor.execute("SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = ?", (i,))
 
@@ -32,6 +32,7 @@ def msSqlInit():
             else:
                 print("Dodaję tabelę", i)
                 cursor.execute(list_of_table_name[i])
+                # TODO Dodać rekordy
                 conn.commit()
 
 
