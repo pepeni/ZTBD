05d8f61b8d60bcbf4e5c71344dfcbc76b121be22
Karmel920 <mikolajpe2@gmail.com>
2024-06-05 00:04:45 +0200

    `if __name__ == "__main__":` added

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,13 @@
+from backend.crimedatapreprocessing.CrimeDataProcessor import CrimeDataProcessor
+from backend.db.PostgreSqlHandler import PostgreSqlHandler
 from backend.dbtimetests.DbOperationsTimeTests import DbOperationsTimeTests
 
-db_operations_timer = DbOperationsTimeTests()
-# db_operations_timer.test_insert_time()
-# db_operations_timer.test_update_time()
-# db_operations_timer.test_delete_time()
-db_operations_timer.test_select_time()
-db_operations_timer.print()
+if __name__ == '__main__':
+    db_operations_timer = DbOperationsTimeTests()
+    # db_operations_timer.test_insert_time()
+    # db_operations_timer.test_update_time()
+    # db_operations_timer.test_delete_time()
+    db_operations_timer.test_select_time()
+    db_operations_timer.print()
+
 
