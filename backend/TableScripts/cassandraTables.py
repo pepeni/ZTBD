022f4b6ac7678dbc941287c43cd413ead0d1fd24
Equilibrium <58709131+Equilibrium23@users.noreply.github.com>
2024-06-06 22:49:03 +0200

    Add cassandra tables

--- backend/TableScripts/cassandraTables.py
+++ backend/TableScripts/cassandraTables.py
@@ -1,25 +1,55 @@
 crimeRegister = '''
     CREATE TABLE IF NOT EXISTS ztbd.CrimeRegister (
-        ID INT PRIMARY KEY,
-        AREA_ID INT,
-        AREA_NAME varchar,
-        CRIME_ID INT,
-        CRIME_DESCRIPTION varchar,
-        VICT_AGE INT,
-        VICT_SEX varchar,
-        VICT_DESCENT varchar,
-        PERMIS_ID INT,
-        PERMIS_DESC varchar,
-        WEAPON_ID INT,
-        WEAPON_DESC varchar,
-        STATUS_ID INT,
-        STATUS_DESC varchar,
-        DR_NO varchar,
-        DATE_RPTD TIMESTAMP,
-        DATE_OCC TIMESTAMP,
-        TIME_OCC varchar,
-        LOCATION varchar,
-        LAT FLOAT,
-        LON FLOAT
-    )
+    ID int,
+    AREA_ID int,
+    CRIME_ID int,
+    VICTIM_ID int,
+    PERMIS_ID int,
+    WEAPON_ID int,
+    STATUS_ID int,
+    DATE_OCC timestamp,
+    LOCATION text,
+    LAT double,
+    LON double,
+    PRIMARY KEY (ID, DATE_OCC)
+)
+'''
+
+joinSelectTable = '''
+    CREATE TABLE IF NOT EXISTS ztbd.JoinSelectTable (
+    STATUS_DESC text PRIMARY KEY,
+    ID int,
+    AREA_ID int,
+    CRIME_ID int,
+    VICTIM_ID int,
+    PERMIS_ID int,
+    WEAPON_ID int,
+    STATUS_ID int,
+    DATE_OCC timestamp,
+    LOCATION text,
+    LAT double,
+    LON double
+)
+'''
+
+complicatedSelectTable = '''
+    CREATE TABLE IF NOT EXISTS ztbd.ComplicatedSelectTable (
+    ID int,
+    AREA_ID int,
+    AREA_NAME text,
+    CRIME_ID int,
+    VICTIM_ID int,
+    VICT_AGE int,
+    VICT_SEX text,
+    VICT_DESCENT text,
+    PERMIS_ID int,
+    WEAPON_ID int,
+    WEAPON_DESC text,
+    STATUS_ID int,
+    DATE_OCC timestamp,
+    LOCATION text,
+    LAT double,
+    LON double,
+    PRIMARY KEY ((AREA_NAME, WEAPON_DESC), ID)
+)
 '''
